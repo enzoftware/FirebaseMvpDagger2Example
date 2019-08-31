642abd4659e32d2626b3b967a1c833f5cf96396d
Enzo Lizama <lizama.enzo@gmail.com>
2019-08-31 17:40:09 -0500

    add commons helpers

---
+++ app/src/main/java/com/upc/monitoringwalkers/common/DialogHelpers.kt
@@ -0,0 +1,2 @@
+package com.upc.monitoringwalkers.common
+
---
+++ app/src/main/java/com/upc/monitoringwalkers/common/ViewExtensions.kt
@@ -0,0 +1,30 @@
+package com.upc.monitoringwalkers.common
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import androidx.viewpager.widget.ViewPager
+
+inline fun EditText.onTextChanged(crossinline onTextChangedHandler: (String?) -> Unit) {
+    addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) = Unit
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+        override fun onTextChanged(input: CharSequence?, start: Int, before: Int, count: Int) {
+            onTextChangedHandler(input?.toString() ?: "")
+        }
+    })
+}
+
+inline fun View.onClick(crossinline onClickHandler: () -> Unit) {
+    setOnClickListener { onClickHandler() }
+}
+
+
+inline fun ViewPager.onPageChange(crossinline onPageChangeHandler: (Int) -> Unit) {
+    addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+        override fun onPageScrollStateChanged(state: Int) = Unit
+        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) = Unit
+        override fun onPageSelected(position: Int) = onPageChangeHandler(position)
+    })
+}
