b6c40a7d713bcaebf65267140b7ebc9e47962b3c
Enzo Lizama <lizama.enzo@gmail.com>
2019-08-31 17:41:16 -0500

    add base activity

--- app/src/main/java/com/upc/monitoringwalkers/ui/base/BaseActivity.kt
+++ app/src/main/java/com/upc/monitoringwalkers/ui/base/BaseActivity.kt
@@ -1,12 +1,21 @@
 package com.upc.monitoringwalkers.ui.base
 
 import android.os.Bundle
-
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         print("Welcome")
+
+    }
+
+    fun setFullScreenMode() {
+        supportActionBar?.hide()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
     }
 }
