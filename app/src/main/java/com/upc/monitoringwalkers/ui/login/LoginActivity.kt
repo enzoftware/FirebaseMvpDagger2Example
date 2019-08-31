ab7d35ef320fa857ddde2c3c95698b3d87784a1c
Enzo Lizama <lizama.enzo@gmail.com>
2019-08-31 13:39:20 -0500

    add login activity

---
+++ app/src/main/java/com/upc/monitoringwalkers/ui/login/LoginActivity.kt
@@ -0,0 +1,46 @@
+package com.upc.monitoringwalkers.ui.login
+
+import android.os.Bundle
+import com.upc.monitoringwalkers.R
+import com.upc.monitoringwalkers.loginPresenter
+import com.upc.monitoringwalkers.ui.base.BaseActivity
+import com.upc.monitoringwalkers.ui.login.view.LoginView
+
+class LoginActivity : BaseActivity(), LoginView {
+
+    private val presenter by lazy { loginPresenter() }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        // Initialize the view
+        presenter.setView(this)
+
+        initUi()
+    }
+
+    private fun initUi() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    //region View Callbacks
+
+    override fun onLoginSuccess() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onLoginError() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showEmailError() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showPasswordError() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    //endregion
+}
