dfd1c9b3addcaff23778e543f367cde230ae2cf5
Enzo Lizama <lizama.enzo@gmail.com>
2019-08-31 17:41:41 -0500

    implement firebase authentication

--- app/src/main/java/com/upc/monitoringwalkers/firebase/authentication/FirebaseAuthenticationInterface.kt
+++ app/src/main/java/com/upc/monitoringwalkers/firebase/authentication/FirebaseAuthenticationInterface.kt
@@ -1,3 +1,14 @@
 package com.upc.monitoringwalkers.firebase.authentication
 
-interface FirebaseAuthenticationInterface
+interface FirebaseAuthenticationInterface {
+    fun login(email: String, password: String, onResult: (Boolean) -> Unit)
+    fun getUserId(): String
+    fun getUsername(): String
+    fun logout(onResult: () -> Unit)
+    fun register(
+        email: String,
+        password: String,
+        username: String,
+        onResult: (Boolean) -> Unit
+    ) // todo : add profile photo
+}
--- app/src/main/java/com/upc/monitoringwalkers/firebase/authentication/FirebaseAuthenticationManager.kt
+++ app/src/main/java/com/upc/monitoringwalkers/firebase/authentication/FirebaseAuthenticationManager.kt
@@ -1,7 +1,43 @@
 package com.upc.monitoringwalkers.firebase.authentication
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import javax.inject.Inject
 
 class FirebaseAuthenticationManager @Inject constructor(private val authentication: FirebaseAuth) :
-    FirebaseAuthenticationInterface
+    FirebaseAuthenticationInterface {
+
+    override fun login(email: String, password: String, onResult: (Boolean) -> Unit) {
+        authentication.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            onResult(it.isComplete && it.isSuccessful)
+        }
+    }
+
+    override fun getUserId(): String = authentication.currentUser?.uid ?: ""
+
+    override fun getUsername(): String = authentication.currentUser?.displayName ?: ""
+
+
+    override fun logout(onResult: () -> Unit) {
+        authentication.signOut()
+        onResult()
+    }
+
+    override fun register(
+        email: String,
+        password: String,
+        username: String,
+        onResult: (Boolean) -> Unit
+    ) {
+        authentication.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+            if (it.isComplete && it.isSuccessful) {
+                authentication.currentUser?.updateProfile(
+                    UserProfileChangeRequest.Builder().setDisplayName(username).build()
+                )
+                onResult(true)
+            } else {
+                onResult(false)
+            }
+        }
+    }
+}
