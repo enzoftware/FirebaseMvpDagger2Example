3c5df22a63df7451ca2c9d4e985850f50913f5c3
Enzo Lizama <lizama.enzo@gmail.com>
2019-09-01 01:51:01 -0500

    add get user type in firebase

--- app/src/main/java/com/upc/monitoringwalkers/firebase/database/FirebaseDatabaseInterface.kt
+++ app/src/main/java/com/upc/monitoringwalkers/firebase/database/FirebaseDatabaseInterface.kt
@@ -1,8 +1,12 @@
 package com.upc.monitoringwalkers.firebase.database
 
+import com.upc.monitoringwalkers.model.DoctorEntity
 import com.upc.monitoringwalkers.model.PatientEntity
 
 interface FirebaseDatabaseInterface {
     fun createPatient(patientEntity: PatientEntity)
     fun getPatientProfile(id: String, onResult: (PatientEntity) -> Unit)
+    fun getDoctorProfile(id: String, onResult: (DoctorEntity) -> Unit)
+    fun getUserType(id: String, onResult: (String) -> Unit)
+//    fun getAdminProfile(id: String, onResult: (DoctorEntity) -> Unit)
 }
--- app/src/main/java/com/upc/monitoringwalkers/firebase/database/FirebaseDatabaseManager.kt
+++ app/src/main/java/com/upc/monitoringwalkers/firebase/database/FirebaseDatabaseManager.kt
@@ -4,6 +4,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.upc.monitoringwalkers.model.DoctorEntity
 import com.upc.monitoringwalkers.model.PatientEntity
 import javax.inject.Inject
 
@@ -12,15 +13,43 @@ private const val KEY_USER = "user"
 class FirebaseDatabaseManager @Inject constructor(private val database: FirebaseDatabase) :
     FirebaseDatabaseInterface {
 
-    override fun getPatientProfile(id: String, onResult: (PatientEntity) -> Unit) {
+    override fun getUserType(id: String, onResult: (String) -> Unit) {
         database.reference.child(KEY_USER).child(id).addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-
+                val userType = snapshot.child("type").value
+                print(userType)
+                onResult(userType.toString())
             }
 
             override fun onCancelled(p0: DatabaseError) = Unit
         })
     }
+
+    override fun getDoctorProfile(id: String, onResult: (DoctorEntity) -> Unit) {
+        database.reference.child(KEY_USER).child(id).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val user = snapshot.getValue(DoctorEntity::class.java)
+                user?.run {
+                    onResult(DoctorEntity(id, name, lastName, password, email, type, username))
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) = Unit
+        })
+    }
+
+    override fun getPatientProfile(id: String, onResult: (PatientEntity) -> Unit) {
+        database.reference.child(KEY_USER).child(id).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val user = snapshot.getValue(PatientEntity::class.java)
+                user?.run {
+                    onResult(PatientEntity(id, name, lastName, email, type, age, treatment))
+                }
+            }
+            override fun onCancelled(p0: DatabaseError) = Unit
+        })
+    }
+
 
     override fun createPatient(patientEntity: PatientEntity) {
         database.reference.child(KEY_USER).child(patientEntity.id).setValue(patientEntity)
