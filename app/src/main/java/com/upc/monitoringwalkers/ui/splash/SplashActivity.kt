f9bbdced56a6ccb5aa3500bdd323aead58d245e7
Enzo Lizama <lizama.enzo@gmail.com>
2019-09-01 01:50:41 -0500

    decide where to go by user type

--- app/src/main/java/com/upc/monitoringwalkers/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/upc/monitoringwalkers/ui/splash/SplashActivity.kt
@@ -4,18 +4,48 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import com.upc.monitoringwalkers.R
+import com.upc.monitoringwalkers.common.shortToast
+import com.upc.monitoringwalkers.splashPresenter
 import com.upc.monitoringwalkers.ui.base.BaseActivity
 import com.upc.monitoringwalkers.ui.login.LoginActivity
+import com.upc.monitoringwalkers.ui.splash.view.SplashView
 
-class SplashActivity : BaseActivity() {
+class SplashActivity : BaseActivity(), SplashView {
+
+    private val presenter by lazy { splashPresenter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setFullScreenMode()
         setContentView(R.layout.activity_splash)
+        presenter.setView(this)
+        initUi()
+    }
+
+    private fun initUi() {
         Handler().postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
+            presenter.decideWhereToGo()
+//            finish()
         }, 3200)
+    }
+
+    override fun onCurrentUserIsDoctor() {
+//        startActivity(Intent(this, LoginActivity::class.java))
+        shortToast(this, "Usuario doctor")
+    }
+
+    override fun onCurrentUserIsPacient() {
+//        startActivity(Intent(this, LoginActivity::class.java))
+        shortToast(this, "Usuario paciente")
+    }
+
+    override fun onCurrentUserIsAdmin() {
+//        startActivity(Intent(this, LoginActivity::class.java))
+        shortToast(this, "Usuario admin")
+    }
+
+    override fun onCurrentUserIsEmpty() {
+        startActivity(Intent(this, LoginActivity::class.java))
+        shortToast(this, "Usuario desconocido")
     }
 }
