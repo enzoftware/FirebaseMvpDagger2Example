2f0c5b8c5adbf73b8406ccb26ebde659665ad0bf
Enzo Lizama <lizama.enzo@gmail.com>
2019-09-01 17:08:16 -0500

    update add doctor xml ui file name

---
+++ app/src/main/java/com/upc/monitoringwalkers/ui/admin/addDoctor/AddDoctorActivity.kt
@@ -0,0 +1,64 @@
+package com.upc.monitoringwalkers.ui.admin.addDoctor
+
+import android.os.Bundle
+import com.upc.monitoringwalkers.R
+import com.upc.monitoringwalkers.addDoctorPresenter
+import com.upc.monitoringwalkers.common.onTextChanged
+import com.upc.monitoringwalkers.common.shortToast
+import com.upc.monitoringwalkers.common.showGeneralError
+import com.upc.monitoringwalkers.ui.admin.addDoctor.view.AddDoctorView
+import com.upc.monitoringwalkers.ui.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_add_doctor.*
+
+class AddDoctorActivity : BaseActivity(), AddDoctorView {
+
+    private val presenter by lazy { addDoctorPresenter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_doctor)
+        presenter.setView(this)
+        initUi()
+    }
+
+    private fun initUi() {
+        register_doctor_email_edit.onTextChanged {
+            presenter.onEmailChanged(it!!)
+        }
+        register_doctor_password_edit.onTextChanged {
+            presenter.onPasswordChanged(it!!)
+        }
+        register_doctor_confirm_password_edit.onTextChanged {
+            presenter.onRepeatPasswordChanged(it!!)
+        }
+        register_doctor_name_edit.onTextChanged {
+            presenter.onNameChanged(it!!)
+        }
+        register_doctor_last_name_edit.onTextChanged {
+            presenter.onLastNameChanged(it!!)
+        }
+        material_button_register_doctor.setOnClickListener {
+            presenter.onRegisterClicked()
+        }
+    }
+
+    override fun onRegisterSuccess() {
+        shortToast(this, "Registro doctor exitoso")
+    }
+
+    override fun showSignUpError() {
+        showGeneralError(this)
+    }
+
+    override fun showEmailError() {
+        register_doctor_email_edit.error = getString(R.string.email_error)
+    }
+
+    override fun showPasswordError() {
+        register_doctor_password_edit.error = getString(R.string.password_error)
+    }
+
+    override fun showPasswordMatchingError() {
+        register_doctor_confirm_password_edit.error = getString(R.string.password_error)
+    }
+}
