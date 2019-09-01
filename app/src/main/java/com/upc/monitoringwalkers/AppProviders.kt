73e0094329e935a227851c41f59a1dbedcf895fd
Enzo Lizama <lizama.enzo@gmail.com>
2019-08-31 20:05:34 -0500

    update app providers for dagger

--- app/src/main/java/com/upc/monitoringwalkers/AppProviders.kt
+++ app/src/main/java/com/upc/monitoringwalkers/AppProviders.kt
@@ -3,3 +3,4 @@ package com.upc.monitoringwalkers
 private fun component() = MonitoringWalkersApp.component
 
 fun loginPresenter() = component().loginPresenter()
+fun registerPresenter() = component().registerPresenter()
