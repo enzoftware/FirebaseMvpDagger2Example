424c7524a5237ca8dcff9a94f8aeb81257499abf
Enzo Lizama <lizama.enzo@gmail.com>
2019-08-31 21:55:21 -0500

    show general error

--- app/src/main/java/com/upc/monitoringwalkers/common/DialogHelpers.kt
+++ app/src/main/java/com/upc/monitoringwalkers/common/DialogHelpers.kt
@@ -1,2 +1,23 @@
 package com.upc.monitoringwalkers.common
 
+import android.content.Context
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.upc.monitoringwalkers.R
+
+fun showGeneralError(context: Context) {
+    AlertDialog.Builder(context)
+        .setTitle(context.resources.getString(R.string.error_title))
+        .setMessage(context.resources.getString(R.string.error_message))
+        .setPositiveButton(context.resources.getString(R.string.accept_button_text))
+        { dialog, _ -> dialog.dismiss() }
+        .show()
+}
+
+fun shortToast(context: Context, text: String) {
+    Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+}
+
+fun longToast(context: Context, text: String) {
+    Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+}
