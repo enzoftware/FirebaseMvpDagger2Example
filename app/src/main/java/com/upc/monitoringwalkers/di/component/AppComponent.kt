eeba43f4288a077d09caa4fccdd4287d9d859185
Enzo Lizama <lizama.enzo@gmail.com>
2019-08-31 20:04:20 -0500

    update di components and modules

--- app/src/main/java/com/upc/monitoringwalkers/di/component/AppComponent.kt
+++ app/src/main/java/com/upc/monitoringwalkers/di/component/AppComponent.kt
@@ -2,11 +2,15 @@ package com.upc.monitoringwalkers.di.component
 
 import com.upc.monitoringwalkers.di.module.PresentationModule
 import com.upc.monitoringwalkers.ui.login.presenter.LoginPresenter
+import com.upc.monitoringwalkers.ui.register.presenter.RegisterPresenter
 import dagger.Component
 import javax.inject.Singleton
 
 @Component(modules = [PresentationModule::class])
 @Singleton
 interface AppComponent {
+
     fun loginPresenter(): LoginPresenter
+
+    fun registerPresenter(): RegisterPresenter
 }
--- app/src/main/java/com/upc/monitoringwalkers/di/module/PresentationModule.kt
+++ app/src/main/java/com/upc/monitoringwalkers/di/module/PresentationModule.kt
@@ -2,6 +2,8 @@ package com.upc.monitoringwalkers.di.module
 
 import com.upc.monitoringwalkers.ui.login.presenter.LoginPresenter
 import com.upc.monitoringwalkers.ui.login.presenter.LoginPresenterImpl
+import com.upc.monitoringwalkers.ui.register.presenter.RegisterPresenter
+import com.upc.monitoringwalkers.ui.register.presenter.RegisterPresenterImpl
 import dagger.Binds
 import dagger.Module
 
@@ -9,4 +11,7 @@ import dagger.Module
 abstract class PresentationModule {
     @Binds
     abstract fun loginPresenter(loginPresenter: LoginPresenterImpl): LoginPresenter
+
+    @Binds
+    abstract fun registerPresenter(registerPresenter: RegisterPresenterImpl): RegisterPresenter
 }
