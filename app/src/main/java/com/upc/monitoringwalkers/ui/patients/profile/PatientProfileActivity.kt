9114c54a7d5d5b088b5cf6cbfd38346d776f178b
Enzo Lizama <lizama.enzo@gmail.com>
2019-09-03 20:07:03 -0500

    dummy patient profile

---
+++ app/src/main/java/com/upc/monitoringwalkers/ui/patients/profile/PatientProfileActivity.kt
@@ -0,0 +1,25 @@
+package com.upc.monitoringwalkers.ui.patients.profile
+
+import android.content.Intent
+import android.os.Bundle
+import com.upc.monitoringwalkers.R
+import com.upc.monitoringwalkers.model.MWCurrentUser
+import com.upc.monitoringwalkers.model.getCurrentUserPreferenceObjectJson
+import com.upc.monitoringwalkers.model.setCurrentUserPreferenceObject
+import com.upc.monitoringwalkers.ui.base.BaseActivity
+import com.upc.monitoringwalkers.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_patient_profile.*
+
+class PatientProfileActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_profile)
+        val currentUser = getCurrentUserPreferenceObjectJson(this)
+        patient_profile_welcome_text.text = "Bienvenido, paciente con codigo ${currentUser.id}"
+        patient_profile_logout_btn.setOnClickListener {
+            setCurrentUserPreferenceObject(this, MWCurrentUser())
+            finish()
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+}
