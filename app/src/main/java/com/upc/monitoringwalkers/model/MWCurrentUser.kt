08891e637a70787feff81a9782cac3dbee2a4d6f
Enzo Lizama <lizama.enzo@gmail.com>
2019-09-03 20:05:07 -0500

    update custom current user with const key

--- app/src/main/java/com/upc/monitoringwalkers/model/MWCurrentUser.kt
+++ app/src/main/java/com/upc/monitoringwalkers/model/MWCurrentUser.kt
@@ -7,23 +7,25 @@ import com.google.gson.Gson
 
 data class MWCurrentUser(val id: String = "", val type: String = "")
 
-fun setCurrentUserPreferenceObject(context: Context, currentUser: MWCurrentUser, key: String) {
+private const val KEY_CURRENT_USER = "currentUser"
+
+fun setCurrentUserPreferenceObject(context: Context, currentUser: MWCurrentUser) {
     val appSharedPrefs = PreferenceManager.getDefaultSharedPreferences(
         context.applicationContext
     )
     val prefsEditor = appSharedPrefs.edit()
     val gson = Gson()
     val jsonObject = gson.toJson(currentUser)
-    prefsEditor.putString(key, jsonObject)
+    prefsEditor.putString(KEY_CURRENT_USER, jsonObject)
     prefsEditor.apply()
 
 }
 
-fun getCurrentUserPreferenceObjectJson(context: Context, key: String): MWCurrentUser {
+fun getCurrentUserPreferenceObjectJson(context: Context): MWCurrentUser {
     val appSharedPrefs = PreferenceManager.getDefaultSharedPreferences(
         context.applicationContext
     )
-    val json = appSharedPrefs.getString(key, "")
+    val json = appSharedPrefs.getString(KEY_CURRENT_USER, "")
     val gson = Gson()
     return if (json != "") gson.fromJson<MWCurrentUser>(json, MWCurrentUser::class.java) else MWCurrentUser()
 }
