0507b213fe08e88214a5829b9e6e2cd4b0326ee3
Enzo Lizama <lizama.enzo@gmail.com>
2019-09-05 12:26:06 -0500

    add new methods to base activity

--- app/src/main/java/com/upc/monitoringwalkers/ui/base/BaseActivity.kt
+++ app/src/main/java/com/upc/monitoringwalkers/ui/base/BaseActivity.kt
@@ -3,8 +3,12 @@ package com.upc.monitoringwalkers.ui.base
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.upc.monitoringwalkers.R
+import de.mrapp.android.dialog.ProgressDialog
 
 abstract class BaseActivity : AppCompatActivity() {
+
+    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +22,19 @@ abstract class BaseActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+    }
+
+
+    fun showLoadingDialog() {
+        val dialogBuilder = ProgressDialog.Builder(this)
+        dialogBuilder.setTitle(getString(R.string.please_wait_txt))
+        dialogBuilder.setMessage(getString(R.string.loading_txt))
+        dialogBuilder.setProgressBarPosition(ProgressDialog.ProgressBarPosition.LEFT)
+        progressDialog = dialogBuilder.create()
+        progressDialog.show()
+    }
+
+    fun hideLoadingDialog() {
+        progressDialog.hide()
     }
 }
