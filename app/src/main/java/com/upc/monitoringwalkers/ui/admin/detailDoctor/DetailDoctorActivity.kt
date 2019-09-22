db3d4760404b934af95ee1c153c41931d2cc47e9
Enzo Lizama <lizama.enzo@gmail.com>
2019-09-22 16:39:26 -0500

    start detail doctor

---
+++ app/src/main/java/com/upc/monitoringwalkers/ui/admin/detailDoctor/DetailDoctorActivity.kt
@@ -0,0 +1,13 @@
+package com.upc.monitoringwalkers.ui.admin.detailDoctor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.upc.monitoringwalkers.R
+
+class DetailDoctorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_doctor)
+    }
+}
